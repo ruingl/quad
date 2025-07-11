0ca1dc780d0d6b9130cdc302ffb1a3b74599844f
ruingl <ruireogo@gmail.com>
2025-07-11 19:34:12 +0800

    quad v1

---
+++ main.py
@@ -0,0 +1,19 @@
+# Quad V1
+# Solves quadratic equations!
+
+import math
+
+print('QuadV1')
+print('Solves quadratic equations!')
+
+k = int(input('Enter value of k: '))
+print()
+
+if k > 0:
+    print(f'Answer: x = ± {math.sqrt(k)}')
+elif k == 0:
+    print(f'Answer: x = 0')
+elif k < 0:
+    print('No real answers or roots.')
+else:
+    print('No real answers or roots.')
